4865c7d0df51ec5a4b567aebab1824ec04299986
Monica Garcia <monicairenegarcia@gmail.com>
2024-06-12 09:39:28 -0700

    complete authenticating users lab

--- server/app.py
+++ server/app.py
@@ -26,12 +26,15 @@ class ClearSession(Resource):
         session['user_id'] = None
 
         return {}, 204
+api.add_resource(ClearSession, '/clear')
 
 class IndexArticle(Resource):
     
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
+    
+api.add_resource(IndexArticle, '/articles')
 
 class ShowArticle(Resource):
 
@@ -48,10 +51,42 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+
+    def post(self):
+        request_dict = request.get_json()
+        user = User.query.filter(User.username == request_dict['username']).first()
+        if user:
+            session['user_id'] = user.id
+            response = make_response(user.to_dict(), 200)
+            return response
+        else:
+            response = make_response("Invalid username", 401)
+            return response
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        response = make_response("", 204)
+        return response
+    
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter_by(id=session['user_id']).first()
+            response = make_response(user.to_dict(), 200)
+            return response
+        else:
+            response = make_response({}, 401)
+            return response
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
